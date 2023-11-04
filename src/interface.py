64bf54e16b43fe9b64e57727f7ee80f5ad0ba459
gavin <gavinwang313@gmail.com>
2023-11-04 07:46:06 +0000

    done

--- src/interface.py
+++ src/interface.py
@@ -1,13 +1,21 @@
+from collections import deque
+
 class Exchange:
     # implement this!
+    balance = 0
 
     def __init__(self, initialBalance):
         """Initial Balance is the amount that each account should start with."""
-        pass
+        self.initialBalance = initialBalance
 
     def add_trade(self, trade):
         """Adds a trade to the exchange (validation required)
         and returns a match if required. It is up to you on how you will
         handle representing trades. """
-        raise NotImplementedError
 
+        if trade.amount < self.initialBalance:
+            return False
+
+        self.balance += trade.amount
+
+        return True
