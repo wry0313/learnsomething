8960fb55f34bab206a279a2d27d107c591ed2e88
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-11-03 23:17:17 -0500

    Revert "step 1 done"
    This reverts commit 1a0e231e207221ae611cabcff894d28f986f1c56.

--- Driver.py
+++ Driver.py
@@ -1,17 +1,6 @@
 import cppimport.import_hook
 import PricingUtil
-import unittest
-
-class TestGetAreaRectangle(unittest.TestCase):
-    def runTest(self):
-        util = PricingUtil.PricingUtil()
-        self.assertAlmostEqual(util.calcVal(1, 0.1, 3), 3, 3, "doesn't work")
-        self.assertAlmostEqual(util.getVal(), 3, 3, "doesn't work")
-        self.assertAlmostEqual(util.calcVal(30, 0.1, 3), 90, 3, "doesn't work")
-        self.assertAlmostEqual(util.getVal(), 90, 3, "doesn't work")
-
 
 
 if __name__ == "__main__":
     print("Nice work on creating the util! Make sure to check the unit tests!")
-    unittest.main()
